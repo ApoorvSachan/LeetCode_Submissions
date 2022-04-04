e960220d914c605fab37d97d1123c0dd95b7da8f
ApoorvSachan <54751426+ApoorvSachan@users.noreply.github.com>
2022-04-05 00:30:20 +0530

    Time: 759 ms (10.99%), Space: 36.5 MB (28.33%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int dp[50][50][50];
-    int f(vector<vector<int>>& grid, int i, int j, int x)
-    {   int y=i+j-x;
+    int dp[50][50][50][50];
+    int f(vector<vector<int>>& grid, int i, int j, int x, int y)
+    {  
         if(x<0 || y<0 || i<0 || j<0 || grid[i][j]==-1 || grid[x][y]==-1)
             return INT_MIN;
         if(i==0 && j==0)
             return grid[i][j];
-     if(dp[i][j][x]!=-1) return dp[i][j][x];
+     if(dp[i][j][x][y]!=-1) return dp[i][j][x][y];
         int pick=0;
-        if(i==x)
+        if(i==x && j==y)
             pick=grid[i][j];
         else
             pick=grid[i][j]+grid[x][y];
-        int ans=f(grid,i,j-1,x);
-        ans=max(ans,f(grid,i-1,j,x-1));
-        ans=max(ans,f(grid,i,j-1,x-1));
-        return dp[i][j][x]=pick+max(ans,f(grid,i-1,j,x));
+        int ans=f(grid,i,j-1,x,y-1);
+        ans=max(ans,f(grid,i-1,j,x-1,y));
+        ans=max(ans,f(grid,i,j-1,x-1,y));
+        return dp[i][j][x][y]=pick+max(ans,f(grid,i-1,j,x,y-1));
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size()-1;
         memset(dp,-1,sizeof(dp));
-        return max(0,f(grid,n,n,n));
+        return max(0,f(grid,n,n,n,n));
     }
 };
